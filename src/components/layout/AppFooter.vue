a92aaffeb0bfe3fb500c4b88af08846b326b8ce1
lse-7660 <lse76609251@gmail.com>
2026-04-14 17:03:03 +0900

    푸터 모바일 수정

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -7,9 +7,9 @@ const isTablet = inject('isTablet', false)
 </script>
 
 <template>
-  <footer class="inline-padding">
-    <div class="footer-text font-prog">CONTACT ME</div>
-    <div class="footer-subtext text-right">
+  <footer class="">
+    <div class="footer-text font-prog inline-padding">CONTACT ME</div>
+    <div class="footer-subtext inline-padding" :class="isMobile ? '' : 'text-right'">
       <p>FROM IDEA TO INTERFACE</p>
       <br />
       <div class="gray-subtext">
@@ -20,34 +20,37 @@ const isTablet = inject('isTablet', false)
         <span>I BUILD WITH CODE</span>
       </div>
     </div>
-    <div class="footer-content-wrap gray-0">
-      <div class="logo-area">
-        <h1 class="blind">이송은 프론트엔드 포트폴리오</h1>
-        <RouterLink to="/main"><img src="/logo-sample.png" alt="logo" /></RouterLink>
-        <div class="name-area">
-          <div class="font-display-small font-medium">이송은</div>
-          <div class="font-label-medium font-light">LEE SONG EUN</div>
+    <div :class="isMobile ? '' : 'inline-padding'">
+      <div class="footer-content-wrap gray-0">
+        <div class="logo-area">
+          <h1 class="blind">이송은 프론트엔드 포트폴리오</h1>
+          <RouterLink to="/main"><img src="/logo-sample.png" alt="logo" /></RouterLink>
+          <div class="name-area">
+            <div class="font-display-small font-medium">이송은</div>
+            <div class="font-label-medium font-light">LEE SONG EUN</div>
+          </div>
         </div>
-      </div>
-      <div
-        :class="`display-area  ${isMobile ? 'font-display-small' : 'font-display-medium text-right'} font-bold`"
-      >
-        STRATEGY<br />
-        UX/UI DESIGN<br />
-        & FRONTEND ENGINEERING
-      </div>
-      <div class="detail-info-area">
-        <p class="detail-info-left">
-          <span class="font-bold">Mail</span
-          ><a href="mailto:thddms355@naver.com">thddms355@naver.com</a>
-          <span class="font-bold">Phone</span><a href="tel:01076609251">010.7660.9251</a>
-        </p>
-        <!-- <p class="detail-info-right">
-          <a href="https://www.jobkorea.co.kr/User/Resume/View?rNo=30343298" target="_blank"
-            >job korea <PhCaretRight
-          /></a>
-          <a href="" target="_blank">saramin <PhCaretRight /></a>
-        </p> -->
+        <div
+          v-if="!isMobile"
+          :class="`display-area  ${isMobile ? 'font-display-small' : 'font-display-medium text-right'} font-bold`"
+        >
+          STRATEGY<br />
+          UX/UI DESIGN<br />
+          & FRONTEND ENGINEERING
+        </div>
+        <div class="detail-info-area">
+          <p class="detail-info-left">
+            <span class="font-bold">Mail</span
+            ><a href="mailto:thddms355@naver.com">thddms355@naver.com</a>
+            <span class="font-bold">Phone</span><a href="tel:01076609251">010.7660.9251</a>
+          </p>
+          <!-- <p class="detail-info-right">
+            <a href="https://www.jobkorea.co.kr/User/Resume/View?rNo=30343298" target="_blank"
+              >job korea <PhCaretRight
+            /></a>
+            <a href="" target="_blank">saramin <PhCaretRight /></a>
+          </p> -->
+        </div>
       </div>
     </div>
   </footer>
@@ -58,11 +61,20 @@ footer {
   border-top: 1px solid var(--gray-border);
   padding-top: var(--space-7);
 }
+footer .mobile-view {
+  padding-top: var(--space-5);
+}
 .footer-text {
   font-size: 6vw;
 }
+.mobile-view .footer-text {
+  font-size: 10vw;
+}
 .footer-subtext {
   padding-block: var(--space-4);
+}
+.mobile-view .footer-subtext {
+  padding-block: var(--space-5);
 }
 .footer-subtext > div {
   display: flex;
